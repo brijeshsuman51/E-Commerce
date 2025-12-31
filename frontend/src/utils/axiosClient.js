2b8ed996c803b5a763480d8f26bde581025e3ac7
Brijesh Suman <brijeshsuman461@gmail.com>
2025-12-31 13:35:02 +0530

    Correction

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -9,10 +9,3 @@ const axiosClient = axios.create({
 });
 
 export default axiosClient;
-
-# Make sure dist/ folder is not in .gitignore
-# Comment out or remove 'dist/' from frontend/.gitignore
-
-git add .
-git commit -m "Added production build and configuration"
-git push origin main
